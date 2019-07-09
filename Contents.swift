9375a3e40eb640e13fc04d1210fb5312fe833549
ellenmcmillan38 <ellenmcmillan38>
2019-07-09 15:11:23 -0500

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var num1 = 1
+var num2 = 2
+var num3 = 3
+
+
+//true or false
+//print(num1<num2)
+//print(num1==num2)
+//
+//print(num1 != num2)
+
+//if num1 + num2 == num3 && num2 + num1 == num3
+//{
+//    print("yay")
+//}
+
+
+
+//if num1 == num2 || num1 == num1 {
+//    print("Yes!")
+//}
+
+//print(5<3)
+//print(12>7)
+//print(6 != 8)
+//print(7 == 7)
+
+//var dogAge = 18
+//
+//if dogAge > 10{
+//    print("Your dog is old")
+//}
+//
+//if dogAge < 2{
+//    print("Aww")
+//}else if dogAge > 10 && dogAge < 15{
+//    print("Cute")
+//}else{
+//    print("sad")
+//}
+
+var favoriteFood = "Starbucks"
+var gross = "Chipotle"
+var fine = "Starbucks"
+
+if favoriteFood == gross{
+    print("Ew why")
+}else if favoriteFood == fine{
+    print("Alright")
+}else{
+    print("Good Job")
+}
